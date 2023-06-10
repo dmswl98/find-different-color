010e448e4ee60cb46174cd96c3bcc0418848725d
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-06-11 03:41:24 +0900

    design: Box hover 스타일 작성

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -19,4 +19,28 @@ const BoxButton = styled.button<Pick<BoxProps, 'size' | 'color'>>`
   margin: 0.2rem;
   background-color: ${({ color }) => color};
   border-radius: 0.4rem;
+
+  -webkit-mask-image: linear-gradient(
+    45deg,
+    #000 25%,
+    rgba(0, 0, 0, 0.2) 50%,
+    #000 75%
+  );
+  mask-image: linear-gradient(
+    45deg,
+    #000 25%,
+    rgba(0, 0, 0, 0.2) 50%,
+    #000 75%
+  );
+  -webkit-mask-size: 800%;
+  mask-size: 800%;
+  -webkit-mask-position: 0;
+  mask-position: 0;
+
+  &:hover {
+    transition: mask-position 2s ease, -webkit-mask-position 2s ease;
+    -webkit-mask-position: 120%;
+    mask-position: 120%;
+    opacity: 1;
+  }
 `;
